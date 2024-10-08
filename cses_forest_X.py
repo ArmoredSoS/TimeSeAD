59d74c182f7081144b3412e4fb960bf286c4a930
Jacopo Sbabo <64830144+ArmoredSoS@users.noreply.github.com>
2024-10-08 13:05:38 +0200

    Script to run the E_X dataset
    Working script for E_X dataset, with "integrated" plotting part

--- cses_forest_X.py
+++ cses_forest_X.py
@@ -1,5 +1,6 @@
 from torch.utils.data import DataLoader
 from timesead.data.cses_dataset_X import CsesDataset
+#from timesead.data.cses_dataset_flat import CsesDataset
 from timesead.models.baselines.iforest import IForestAD
 import matplotlib.pyplot as plot
 import os
@@ -11,38 +12,30 @@ import numpy
 
 def main():
     cses_train_ds = CsesDataset()
-    cses_test_ds = CsesDataset(training=False)
-
     cses_train_dl = DataLoader(cses_train_ds)
-    cses_test_dl = DataLoader(cses_test_ds)
 
-    forest = IForestAD(n_trees=100, max_samples=256, max_features=0.5, bootstrap=False, input_shape= 'btf')
-
+    forest = IForestAD(n_trees=100, max_features=0.5, bootstrap=False, input_shape= 'btf')
     forest.model.contamination = 0.5
     forest.fit(cses_train_dl)
+
+    #cses_test_ds = CsesDataset(training=False)
+    #cses_test_dl = DataLoader(cses_test_ds)
 
     plots_dir = 'Plots'
     if not os.path.exists(plots_dir):
         os.makedirs(plots_dir)
 
-    #all_scores = []
-    #series = []
-
-    for item, idx in cses_test_dl:
+    for item, idx in cses_train_dl:
         scores = forest.compute_online_anomaly_score(item)
         scores = scores.numpy()
-        #all_scores.extend(scores.numpy())
-        series = item.numpy().squeeze()
-
-        #series.extend(item.numpy())
-        #series = numpy.array(series).squeeze()
+        series = forest.format_online_targets(item)
 
         plot.figure(figsize=(12, 6))
-        plot.plot(series.flatten(), label='E_X_res', color='blue', alpha=0.6)
+        plot.plot(series, label='E_X_normalized', color='blue', alpha=0.6)
         plot.axhline(y=forest.model.threshold_, color='red', linestyle='--', label='Threshold')
         
         anomalies = numpy.where(scores > forest.model.threshold_)[0]
-        plot.scatter(anomalies, series.flatten()[anomalies], color='red', label='Anomalies')
+        plot.scatter(anomalies, series[anomalies], color='red', label='Anomalies', s=15)
 
         plot.title('Test')
         plot.xlabel('Time')
@@ -50,11 +43,6 @@ def main():
 
         plot.savefig(os.path.join(plots_dir, f'{idx + 1}.png'))
         plot.close() 
-
-    #plt.figure(figsize=(10, 6))
-    #sns.histplot(scores, bins=50, kde=True)
-    #plt.title(f'Threshold: {forest.model.threshold_}')
-    #plt.show()
 
 if __name__ == '__main__':
     main()
